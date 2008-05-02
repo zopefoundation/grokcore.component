93bd626ac03fcde19eb9fa6b2813566ae8af0ae9
Philipp von Weitershausen <philipp@weitershausen.de>
2008-05-02 09:59:19 +0000

    Set version number on tag.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ long_description = (
 
 setup(
     name='grokcore.component',
-    version='1.1dev',
+    version='1.0.1',
     author='Grok Team',
     author_email='grok-dev@zope.org',
     url='http://grok.zope.org',
