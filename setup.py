33d1aaa36766c2afbc637d26cfe1098ee7a3f330
Philipp von Weitershausen <philipp@weitershausen.de>
2008-05-04 15:14:45 +0000

    Remove dev marker on release.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ long_description = (
 
 setup(
     name='grokcore.component',
-    version='1.2dev',
+    version='1.2',
     author='Grok Team',
     author_email='grok-dev@zope.org',
     url='http://grok.zope.org',
