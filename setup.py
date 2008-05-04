2e5f331a8381d5a9fa07c7441d1cf350d58aa930
Philipp von Weitershausen <philipp@weitershausen.de>
2008-05-04 15:00:39 +0000

    Require *at least* martian 0.9.4.

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=['setuptools',
-                      'martian >= 0.9.3',
+                      'martian >= 0.9.4',
                       'zope.component',
                       'zope.configuration',
                       'zope.interface',
