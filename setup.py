acfbfd2adfe646736adc05b8399cf67065d0c326
Philipp von Weitershausen <philipp@weitershausen.de>
2008-07-22 14:29:35 +0000

    Version number wasn't bumped after last release. Do that now.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ long_description = (
 
 setup(
     name='grokcore.component',
-    version='1.4dev',
+    version='1.5dev',
     author='Grok Team',
     author_email='grok-dev@zope.org',
     url='http://grok.zope.org',
