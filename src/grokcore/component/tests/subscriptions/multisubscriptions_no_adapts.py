dd2e58f9448b8f036edb38d8878152dbabf2f1ae
Jan-Wijbrand Kolman <janwijbrand@gmail.com>
2011-02-14 10:16:31 +0000

    change test name to reflect actually error being tested

--- src/grokcore/component/tests/subscriptions/multisubscriptions_no_adapts.py
+++ src/grokcore/component/tests/subscriptions/multisubscriptions_no_adapts.py
@@ -2,7 +2,7 @@
   >>> grok.testing.grok(__name__)
   Traceback (most recent call last):
     ...
-  GrokError: <class 'grokcore.component.tests.subscriptions.multisubscriptions_no_context.CaveGardenRenovator'>
+  GrokError: <class 'grokcore.component.tests.subscriptions.multisubscriptions_no_adapts.CaveGardenRenovator'>
   must specify which contexts it adapts (use the 'adapts' directive to specify).
 
 """
