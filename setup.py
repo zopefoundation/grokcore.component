d3fa5cf9c4d77fe44afb71d0be44ad9c94f5acf7
Jan-Wijbrand Kolman <janwijbrand@gmail.com>
2011-02-14 10:33:02 +0000

    Back to development: 2.4

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ tests_require = [
 
 setup(
     name='grokcore.component',
-    version='2.3',
+    version='2.4dev',
     author='Grok Team',
     author_email='grok-dev@zope.org',
     url='http://grok.zope.org',
