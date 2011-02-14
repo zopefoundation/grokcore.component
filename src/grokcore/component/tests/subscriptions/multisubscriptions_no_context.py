f50c4616ced8516da9cc5a6a7d97825515c8494f
Jan-Wijbrand Kolman <janwijbrand@gmail.com>
2011-02-14 10:07:58 +0000

    add two simple tests for no context and no interface for multi subscriptions

---
+++ src/grokcore/component/tests/subscriptions/multisubscriptions_no_context.py
@@ -0,0 +1,19 @@
+"""
+  >>> grok.testing.grok(__name__)
+  Traceback (most recent call last):
+    ...
+  GrokError: <class 'grokcore.component.tests.subscriptions.multisubscriptions_no_context.CaveGardenRenovator'>
+  must specify which contexts it adapts (use the 'adapts' directive to specify).
+
+"""
+
+import grokcore.component as grok
+from zope import interface
+
+class IRenovate(interface.Interface):
+
+    def takedown():
+        pass
+
+class CaveGardenRenovator(grok.MultiSubscription):
+    grok.provides(IRenovate)
---
+++ src/grokcore/component/tests/subscriptions/multisubscriptions_no_interface.py
@@ -0,0 +1,20 @@
+"""
+  >>> grok.testing.grok(__name__)
+  Traceback (most recent call last):
+    ...
+  GrokError: <class 'grokcore.component.tests.subscriptions.multisubscriptions_no_interface.CaveGardenRedecorator'>
+  must implement at least one interface (use grok.implements to specify).
+
+"""
+
+import grokcore.component as grok
+
+
+class Cave(grok.Context):
+    pass
+
+class Garden(grok.Context):
+    pass
+
+class CaveGardenRedecorator(grok.MultiSubscription):
+    pass
