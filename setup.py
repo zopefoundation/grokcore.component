eda79a4506a98c8934d91a883be07f500fd2e3d9
Jan-Wijbrand Kolman <janwijbrand@gmail.com>
2011-04-27 09:17:08 +0000

    Preparing release 2.4

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ tests_require = [
 
 setup(
     name='grokcore.component',
-    version='2.4dev',
+    version='2.4',
     author='Grok Team',
     author_email='grok-dev@zope.org',
     url='http://grok.zope.org',
