0bcdd6c493d66446cb71504d8316750007a812b9
Michael Howitz <mh@gocept.com>
2016-02-14 20:48:43 +0100

    Add ability to exclude more than one module or package using ``<grok:grok exclude="<names>" />`` and allow to use unix shell-style wildcards within.

---
+++ src/grokcore/component/tests/zcml/excludemany.py
@@ -0,0 +1,50 @@
+"""
+It allows to exclude a many packages or modules from beeing grokked.
+
+These packages or modules can be specified using unix shell-style wildcards
+
+There is a NameError in `.excludemanypkg.file_1` which is raised when this
+module is not excluded:
+
+>>> xmlconfig.string('''
+...     <configure xmlns:grok="http://namespaces.zope.org/grok">
+...       <include package="grokcore.component" file="meta.zcml"/>
+...       <grok:grok package="." />
+...     </configure>''', context)
+Traceback (most recent call last):
+ZopeXMLConfigurationError: File "<string>", line 4.6-4.31
+    NameError: name 'asdf' is not defined
+
+There is a NameError in `.excludemanypkg.test_asdf`, too which is raised when
+this module is not excluded:
+
+>>> xmlconfig.string('''
+...     <configure xmlns:grok="http://namespaces.zope.org/grok">
+...       <include package="grokcore.component" file="meta.zcml"/>
+...       <grok:grok package="."
+...                  exclude="file_*" />
+...     </configure>''', context)
+Traceback (most recent call last):
+ZopeXMLConfigurationError: File "<string>", line 4.6-5.36
+    NameError: name 'qwe' is not defined
+
+
+Excluding both 'file_1` and `test_asdf`allows to successfully grok the module:
+
+>>> xmlconfig.string('''
+...     <configure xmlns:grok="http://namespaces.zope.org/grok">
+...       <include package="grokcore.component" file="meta.zcml"/>
+...       <grok:grok package="."
+...                  exclude="file_*
+...                           *asdf" />
+...     </configure>''', context)
+<zope.configuration.config.ConfigurationMachine ...>
+
+"""
+
+from zope.configuration import xmlconfig
+from grokcore.component.tests.zcml import excludemanypkg
+
+context = xmlconfig.ConfigurationMachine()
+xmlconfig.registerCommonDirectives(context)
+context.package = excludemanypkg
---
+++ src/grokcore/component/tests/zcml/excludemanypkg/test_asdf.py
@@ -0,0 +1,1 @@
+qwe  # This leads to a NameError if exclude does not work correctly.
