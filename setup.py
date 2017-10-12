5d56238dfab094f108a656a12d57d5f1aa3f3c88
Michael Howitz <mh@gocept.com>
2017-10-12 08:30:25 +0200

    Add support for Python 3.5, 3.6, PyPy2 and PyPy3.
    Drop support for Python 2.6 and 3.3.

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
+
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
@@ -8,32 +9,35 @@ long_description = (
     read('README.rst')
     + '\n' +
     read('CHANGES.rst')
-    )
+)
 
 tests_require = [
     'zope.event',
-    ]
+]
 
 setup(
     name='grokcore.component',
-    version='2.8.dev0',
+    version='3.0.dev0',
     author='Grok Team',
     author_email='grok-dev@zope.org',
     url='http://grok.zope.org',
-    download_url='http://pypi.python.org/pypi/grokcore.component',
+    download_url='https://github.com/zopefoundation/grokcore.component',
     description='Grok-like configuration for basic components '
                 '(adapters, utilities, subscribers)',
     long_description=long_description,
     license='ZPL',
     classifiers=['Intended Audience :: Developers',
+                 'Development Status :: 6 - Mature',
                  'License :: OSI Approved :: Zope Public License',
-                 'Programming Language :: Python',
+                 'Operating System :: OS Independent',
                  'Programming Language :: Python :: 2',
-                 'Programming Language :: Python :: 2.6',
                  'Programming Language :: Python :: 2.7',
                  'Programming Language :: Python :: 3',
-                 'Programming Language :: Python :: 3.3',
                  'Programming Language :: Python :: 3.4',
+                 'Programming Language :: Python :: 3.5',
+                 'Programming Language :: Python :: 3.6',
+                 'Programming Language :: Python :: Implementation :: CPython',
+                 'Programming Language :: Python :: Implementation :: PyPy',
                  ],
 
     packages=find_packages('src'),
