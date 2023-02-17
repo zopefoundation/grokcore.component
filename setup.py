21629f8654961d1b4939bca8ad6b8463c13a6574
Michael Howitz <mh@gocept.com>
2023-02-17 08:04:23 +0100

    Back to development: 4.1

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ tests_require = [
 
 setup(
     name='grokcore.component',
-    version='4.0',
+    version='4.1.dev0',
     author='Grok Team',
     author_email='zope-dev@zope.dev',
     url='https://github.com/zopefoundation/grokcore.component',
